267fc1924533d60472d4c3aadc930e84753be1f1
sujana.saragadam <sujana.saragadam@kony.com>
2015-08-18 00:08:25 +0700

    commit changes

--- project_properties.p
+++ project_properties.p
@@ -32,7 +32,7 @@ Vhttp://mission-rnd.github.io/javascript-basics.htm
 p16
 sVproject_status
 p17
-I3
+I4
 sVproject_gitrepo
 p18
 Vhttps://github.com/mission-rnd/mrnd-javascript
--- src/Objects.js
+++ src/Objects.js
@@ -1,17 +1,27 @@
 exports.GetObjectPropertyNames = function(obj) {
-	var keysarray = Object.keys(obj);
-	return keysarray;	
+    if(obj!=null)
+    {
+    	var keysarray = Object.keys(obj);
+    	return keysarray;	
+    }
+    else
+        return null;
 }
 
 exports.GetObjectPropertyValues = function(obj) {
-	var keysarray = Object.keys(obj);
-    var keysObjects;
-    if(keysarray.length>0)
+    if(obj!=null)
     {
-        keysObjects = [];
-        for(var i=0; i<keysarray.length;i++)
-            keysObjects.push(obj[keysarray[i]]);
+    	var keysarray = Object.keys(obj);
+        var keysObjects;
+        if(keysarray.length>0)
+        {
+            keysObjects = [];
+            for(var i=0; i<keysarray.length;i++)
+                keysObjects.push(obj[keysarray[i]]);
+        }
+        return keysObjects;
     }
-    return keysObjects;
+    else
+        return null;
 }
 
